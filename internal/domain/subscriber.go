ab085111e098f79dc869e8c641df78c5a361eaaa
Akansha Roy <akansharoy2008@gmail.com>
2026-04-14 14:35:35 +0530

    feat : subscriber domain that maps to db

--- internal/domain/subscriber.go
+++ internal/domain/subscriber.go
@@ -1,1 +1,13 @@
 package domain
+
+import "time"
+
+type Subscriber struct {
+	ID             string    `db:"id"`
+	Email          string    `db:"email"`
+	Confirmed      bool      `db:"confirmed"`
+	Token          string    `db:"token"`
+	TokenExpiresAt time.Time `db:"token_expires_at"`
+	CreatedAt      time.Time `db:"created_at"`
+	UpdatedAt      time.Time `db:"updated_at"`
+}

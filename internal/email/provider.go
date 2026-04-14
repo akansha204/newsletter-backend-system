52f5b8d2efd19adadb436aeae9a6cc2d1b55efd4
Akansha Roy <akansharoy2008@gmail.com>
2026-04-14 22:44:54 +0530

    demo email service

--- internal/email/provider.go
+++ internal/email/provider.go
@@ -1,1 +1,11 @@
 package email
+
+type Provider interface {
+	Send(to, subject, body string) error
+}
+
+type Message struct {
+	To      string
+	Subject string
+	Body    string
+}
--- internal/email/sendgrid.go
+++ internal/email/sendgrid.go
@@ -1,1 +1,20 @@
 package email
+
+import (
+	"fmt"
+	"log"
+)
+
+type SendGridProvider struct {
+	apiKey string
+}
+
+func NewSendGridProvider(apiKey string) *SendGridProvider {
+	return &SendGridProvider{apiKey: apiKey}
+}
+
+func (s *SendGridProvider) Send(to, subject, body string) error {
+	log.Printf("[EMAIL] to=%s subject=%s body=%s", to, subject, body)
+	fmt.Printf("\n Sending email to: %s\n   Subject: %s\n   Body: %s\n\n", to, subject, body)
+	return nil
+}

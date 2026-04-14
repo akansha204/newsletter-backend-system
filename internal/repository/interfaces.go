c7c0f39af0cdc23df9f1a2edffb439d5528c8af9
Akansha Roy <akansharoy2008@gmail.com>
2026-04-14 14:39:07 +0530

    feat: repository layer of subscriber operations

--- internal/repository/interfaces.go
+++ internal/repository/interfaces.go
@@ -1,1 +1,11 @@
 package repository
+
+import "github.com/akansh204/newsletter-backend-system/internal/domain"
+
+type SubscriberRepository interface {
+	Create(subscriber *domain.Subscriber) error
+	FindByEmail(email string) (*domain.Subscriber, error)
+	FindByToken(token string) (*domain.Subscriber, error)
+	Confirm(id string) error
+	FindAllConfirmed() ([]domain.Subscriber, error)
+}
